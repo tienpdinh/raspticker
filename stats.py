4c2863b9bbb8992b7672a5c31675b6bb640fe225
Tien Dinh <tiendinhphuc@gmail.com>
2023-07-17 22:29:56 -0500

    added temp

--- stats.py
+++ stats.py
@@ -75,13 +75,15 @@ while True:
 
     # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
     cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True )
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True )
+    IP = subprocess.check_output(cmd, shell = True)
+    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f%\", $(NF-2)}'"
+    CPU = subprocess.check_output(cmd, shell = True)
     cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True )
+    MemUsage = subprocess.check_output(cmd, shell = True)
     cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True )
+    Disk = subprocess.check_output(cmd, shell = True)
+    cmd = "vcgencmd measure_temp | cut -d '=' -f2"
+    Temp = subprocess.check_output(cmd, shell = True)
 
     # Write two lines of text.
 
@@ -89,6 +91,7 @@ while True:
     draw.text((x, top+8),     CPU.decode('ascii'), font=font, fill=255)
     draw.text((x, top+16),    MemUsage.decode('ascii'),  font=font, fill=255)
     draw.text((x, top+25),    Disk.decode('ascii'),  font=font, fill=255)
+    draw.text((x, top+33),    "CPU Temp: "+ Temp.decode('ascii'), font=font, fill=255)
 
     # Display image.
     disp.image(image)
