793c5a8b5c38cb3ef0005e0717326e180b96e743
Tien Dinh <tiendinhphuc@gmail.com>
2023-07-17 22:56:34 -0500

    ...

--- stats.py
+++ stats.py
@@ -68,30 +68,45 @@ x = 0
 
 font = ImageFont.truetype('assets/Consolas.ttf', 10)
 
+# Feature toggle to display pihole info
+pihole = False
+
 while True:
 
     # Draw a black filled box to clear the image.
     draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-    # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
-    cmd = "hostname -I | cut -d\' \' -f1"
-    IP = subprocess.check_output(cmd, shell = True)
-    cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
-    CPU = subprocess.check_output(cmd, shell = True)
-    cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
-    MemUsage = subprocess.check_output(cmd, shell = True)
-    cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
-    Disk = subprocess.check_output(cmd, shell = True)
-    cmd = "vcgencmd measure_temp | cut -d '=' -f2"
-    Temp = subprocess.check_output(cmd, shell = True)
+    if not button_A.value:
+        pihole = not pihole
 
-    # Write two lines of text.
+    if pihole:
+        cmd = 'pihole version | head -n 1 | cut -d ' ' -f6'
+        PiHoleVersion = subprocess.check_output(cmd, shell = True)
+        cmd = 'pihole status'
+        PiHoleStatus = subprocess.check_output(cmd, shell = True)
 
-    draw.text((x, top),       "IP: " + IP.decode('ascii'),  font=font, fill=255)
-    draw.text((x, top+8),     CPU.decode('ascii') + '%', font=font, fill=255)
-    draw.text((x, top+16),    MemUsage.decode('ascii'),  font=font, fill=255)
-    draw.text((x, top+25),    Disk.decode('ascii'),  font=font, fill=255)
-    draw.text((x, top+33),    "CPU Temp: "+ Temp.decode('ascii'), font=font, fill=255)
+        draw.text((x, top),       "Version: " + PiHoleVersion.decode('ascii'),  font=font, fill=255)
+        draw.text((x, top+8),     PiHoleStatus.decode('ascii') + '%', font=font, fill=255)
+    else:
+        # Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+        cmd = "hostname -I | cut -d\' \' -f1"
+        IP = subprocess.check_output(cmd, shell = True)
+        cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+        CPU = subprocess.check_output(cmd, shell = True)
+        cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+        MemUsage = subprocess.check_output(cmd, shell = True)
+        cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+        Disk = subprocess.check_output(cmd, shell = True)
+        cmd = "vcgencmd measure_temp | cut -d '=' -f2"
+        Temp = subprocess.check_output(cmd, shell = True)
+
+        # Write two lines of text.
+
+        draw.text((x, top),       "IP: " + IP.decode('ascii'),  font=font, fill=255)
+        draw.text((x, top+8),     CPU.decode('ascii') + '%', font=font, fill=255)
+        draw.text((x, top+16),    MemUsage.decode('ascii'),  font=font, fill=255)
+        draw.text((x, top+25),    Disk.decode('ascii'),  font=font, fill=255)
+        draw.text((x, top+33),    "CPU Temp: "+ Temp.decode('ascii'), font=font, fill=255)
 
     # Display image.
     disp.image(image)
