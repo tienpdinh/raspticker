d0981f4b10a5bd322908acc32b58a0e6a2c72513
Tien Dinh <tiendinhphuc@gmail.com>
2023-08-04 22:30:06 -0500

    more details

--- ticker.py
+++ ticker.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageFont, ImageDraw, ImageOps
 from FinancialModelingPrepApi import FinancialModelingPrepApi
 import asyncio
 import secret
-from datetime import datetime, timezone
+from datetime import datetime
 
 inky_display = auto()
 inky_display.set_border(inky_display.WHITE)
@@ -14,22 +14,27 @@ async def main():
     while True:
         img = Image.new("P", (inky_display.WIDTH, inky_display.HEIGHT))
         draw = ImageDraw.Draw(img)
-
         font = ImageFont.truetype('./assets/Consolas.ttf', 22)
-        price = await fmp.get_stock_price(ticker)
+        price = str(await fmp.get_stock_price(ticker))
+        now = datetime.now()
+        now_str = 'Updated: ' + now.strftime("%m/%d/%Y, %H:%M:%S")
         ticker_w, ticker_h = font.getsize(ticker)
-        price_w, price_h = font.getsize(str(price))
+        price_w, price_h = font.getsize(price)
+        now_w, now_h = font.getsize(now_str)
         ticker_x = (inky_display.WIDTH / 2) - (ticker_w / 2)
-        ticker_y = (inky_display.HEIGHT / 2) - (ticker_h / 2) - ticker_h
+        ticker_y = (inky_display.HEIGHT / 3)
         price_x = (inky_display.WIDTH / 2) - (price_w / 2)
-        price_y = (inky_display.HEIGHT / 2) - (price_h / 2) + price_h
+        price_y = (inky_display.HEIGHT / 2) - (price_h / 2)
+        now_x = (inky_display.WIDTH / 2) - (ticker_w / 2)
+        now_y = (2 * inky_display.HEIGHT / 3)
 
         draw.text((ticker_x, ticker_y), ticker, inky_display.BLACK, font)
-        draw.text((price_x, price_y), str(price), inky_display.BLACK, font)
+        draw.text((price_x, price_y), '$' + price, inky_display.YELLOW, font)
+        draw.text((price_x, price_y), now_str, inky_display.BLACK, font)
         inky_display.set_image(img)
         inky_display.show()
 
-        print(f'Updated at {datetime.now(timezone.utc)}!')
-        await asyncio.sleep(5)
+        print(f'Updated at {datetime.now()}!')
+        await asyncio.sleep(6*60)
 
 asyncio.run(main())
